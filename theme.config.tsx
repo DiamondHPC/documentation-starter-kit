f77b5e28e3de8141a6912123eaaafebfa1b9ee35
Minu Mathew <minumpmathews@gmail.com>
2025-03-06 14:41:16 -0600

    index page changes

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,13 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Diamond Project Documentation</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/DiamondHPC/documentation-starter-kit',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/DiamondHPC/documentation-starter-kit',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Diamond Documentation',
   },
 }
 
